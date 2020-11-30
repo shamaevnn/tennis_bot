11e582d594cdf0cb55150e7c3c1d999eeb1f9d21
nikita <shamaev090299@gmail.com>
2020-11-30 18:27:12 +0300

    fix periodic task

--- base/periodic_tasks.py
+++ base/periodic_tasks.py
@@ -5,7 +5,7 @@ django.setup()
 
 from django.db.models import ExpressionWrapper, F, DurationField
 from base.models import AlertsLog, GroupTrainingDay
-from base.utils import moscow_datetime, send_message, get_time_info_from_tr_day
+from base.utils import moscow_datetime, get_time_info_from_tr_day
 from datetime import datetime, timedelta
 from tennis_bot.config import TELEGRAM_TOKEN
 from tele_interface.manage_data import ALERT_TEXTS
@@ -18,10 +18,13 @@ def send_alert_about_coming_train():
     alert_log_tr_days = list(AlertsLog.objects.filter(
         is_sent=True, alert_type=AlertsLog.COMING_TRAIN).values_list("tr_day_id", flat=True).distinct())
 
-    tr_days = GroupTrainingDay.objects.annotate(diff=ExpressionWrapper(
-        F('start_time') + F('date') - moscow_datetime(datetime.now()),
-        output_field=DurationField())).filter(date__gte=moscow_datetime(datetime.now()),
-                                              diff__lte=timedelta(hours=5)).exclude(id__in=alert_log_tr_days).distinct()
+    tr_days = GroupTrainingDay.objects.annotate(
+        diff=ExpressionWrapper(F('start_time') + F('date') - moscow_datetime(datetime.now()),
+                               output_field=DurationField()),
+        train_dttm=ExpressionWrapper(F('start_time') + F('date'),
+                                     output_field=DurationField())).filter(train_dttm__gte=moscow_datetime(datetime.now()),
+                                                                           diff__lte=timedelta(hours=5),
+                                                                           is_available=True).exclude(id__in=alert_log_tr_days).distinct()
 
     for tr_day in tr_days:
         group_members = tr_day.group.users.all()
@@ -53,7 +56,7 @@ def send_alert_about_coming_train():
                 AlertsLog.objects.create(is_sent=False, player=player, tr_day=tr_day, alert_type=AlertsLog.COMING_TRAIN)
 
 
-schedule.every(2).seconds.do(send_alert_about_coming_train)
+schedule.every(59).minutes.do(send_alert_about_coming_train)
 
 while 1:
     schedule.run_pending()

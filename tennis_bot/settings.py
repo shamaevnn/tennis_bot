c9a17cb89488077fc4ce932e2cf53148996590a4
nikita <shamaev090299@gmail.com>
2020-12-07 16:27:12 +0300

    sentry

--- tennis_bot/settings.py
+++ tennis_bot/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 from tennis_bot.config import SECRET_KEY_DJANGO
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

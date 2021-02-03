01d8a06365501446b013619b4e03d4b8d23b73b3
nikita <shamaev090299@gmail.com>
2021-02-04 02:20:52 +0300

    fix

--- tele_interface/utils.py
+++ tele_interface/utils.py
@@ -201,15 +201,17 @@ def get_potential_days_for_group_training(user):
         Count('visitors', distinct=True),
         Count('pay_visitors', distinct=True),
         max_players=F('group__max_players'),
-        diff=ExpressionWrapper(F('start_time') + F('date') - moscow_datetime(datetime.now()),
-                               output_field=DurationField())).filter(
-        Q(max_players__gt=F('pay_visitors__count')+F('visitors__count')+F('group__users__count')-F('absent__count')) |
-        (Q(max_players__lte=F('pay_visitors__count')+F('visitors__count')+F('group__users__count')-F('absent__count')) &
-            Q(group__available_for_additional_lessons=True) & Q(max_players__lt=6)),
-                diff__gte=timedelta(hours=1),
-                group__status=TrainingGroup.STATUS_GROUP,
-        ).exclude(
-            Q(visitors__in=[user]) | Q(group__users__in=[user])).order_by('start_time')
+                    diff=ExpressionWrapper(F('start_time') + F('date') - moscow_datetime(datetime.now()),
+                               output_field=DurationField()))\
+            .annotate(all_users=F('pay_visitors__count')+F('visitors__count')+
+                                F('group__users__count')-F('absent__count')).filter(
+            Q(max_players__gt=F('all_users')) |
+                (Q(max_players__lte=F('all_users')) & Q(group__available_for_additional_lessons=True) &
+                 Q(max_players__lt=6) & Q(all_users__lt=6)),
+                        diff__gte=timedelta(hours=1),
+                        group__status=TrainingGroup.STATUS_GROUP,
+                ).exclude(
+                    Q(visitors__in=[user]) | Q(group__users__in=[user])).order_by('start_time')
 
     return potential_free_places
 

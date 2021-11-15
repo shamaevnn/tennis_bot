1563b47cbb7a3f77e3a89f922603ebb995cc4ff0
nikita <shamaev090299@gmail.com>
2021-11-15 23:33:57 +0300

    init

--- admin_bot/view_schedule/handlers.py
+++ admin_bot/view_schedule/handlers.py
@@ -1,37 +1,43 @@
-import base.common_for_bots.static_text
 from admin_bot.view_schedule.keyboards import back_from_show_grouptrainingday_info_keyboard
 from admin_bot.view_schedule import static_text
 from admin_bot.view_schedule.manage_data import SHOW_GROUPDAY_INFO
 from admin_bot.view_schedule.utils import schedule_users_info
+from base.common_for_bots.static_text import DATE_INFO
 from base.models import GroupTrainingDay, TrainingGroup
 from base.common_for_bots.utils import bot_edit_message, get_time_info_from_tr_day
 
 
 def show_trainingroupday_info(update, context):
     tr_day_id = update.callback_query.data[len(SHOW_GROUPDAY_INFO):]
-    tr_day = GroupTrainingDay.objects.select_related('group').prefetch_related('visitors', 'pay_visitors', 'pay_bonus_visitors').get(id=tr_day_id)
+    tr_day: GroupTrainingDay = GroupTrainingDay.objects.select_related(
+        'group'
+    ).prefetch_related(
+        'visitors', 'pay_visitors', 'pay_bonus_visitors'
+    ).get(id=tr_day_id)
 
     availability = f'{static_text.NO_TRAIN}\n' if not tr_day.is_available else ''
+
     is_individual = f'{static_text.INDIVIDUAL_TRAIN}\n' if tr_day.is_individual else f'{static_text.GROUP_TRAIN}️\n'
     affiliation = f'{static_text.MY_TRAIN}\n\n' if tr_day.tr_day_status == GroupTrainingDay.MY_TRAIN_STATUS else f'{static_text.RENT}\n\n'
 
     group_name = f"{tr_day.group.name}\n"
 
-    if not tr_day.is_individual:
+    if tr_day.is_individual or tr_day.tr_day_status == GroupTrainingDay.RENT_COURT_STATUS:
+        group_players, visitors, pay_visitors, pay_bonus_visitors, absents, group_level = '', '', '', '', '', ''
+    else:
         group_players = f'{static_text.PLAYERS_FROM_GROUP}:\n{schedule_users_info(tr_day.group.users.all().difference(tr_day.absent.all()))}\n'
         visitors = f'\n{static_text.HAVE_COME_FROM_OTHERS}:\n{schedule_users_info(tr_day.visitors)}\n' if tr_day.visitors.exists() else ''
         pay_visitors = f'\n{static_text.HAVE_COME_FOR_MONEY}:\n{schedule_users_info(tr_day.pay_visitors)}\n' if tr_day.pay_visitors.exists() else ''
         pay_bonus_visitors = f'\n{static_text.HAVE_COME_FOR_PAY_BONUS_LESSON}:\n{schedule_users_info(tr_day.pay_bonus_visitors)}\n' if tr_day.pay_bonus_visitors.exists() else ''
         absents = f'\n{static_text.ARE_ABSENT}:\n{schedule_users_info(tr_day.absent)}\n' if tr_day.absent.exists() else ''
         group_level = f"{TrainingGroup.GROUP_LEVEL_DICT[tr_day.group.level]}\n"
-    else:
-        group_players, visitors, pay_visitors, pay_bonus_visitors, absents, group_level = '', '', '', '', '', ''
 
     time_tlg, _, _, date_tlg, day_of_week, _, _ = get_time_info_from_tr_day(tr_day)
+    date_info = DATE_INFO.format(date_tlg, day_of_week, time_tlg)
 
-    general_info = f'<b>{date_tlg} ({day_of_week})\n{time_tlg}</b>\n{availability}{is_individual}{affiliation}'
+    general_info = f'{availability}{is_individual}{affiliation}'
     users_info = f'{group_name}{group_level}{group_players}{visitors}{pay_visitors}{pay_bonus_visitors}{absents}'
-    text = f'{general_info}{users_info}'
+    text = f'{general_info}{users_info}{date_info}'
 
     markup = back_from_show_grouptrainingday_info_keyboard(
         year=tr_day.date.year,
--- admin_bot/view_schedule/utils.py
+++ admin_bot/view_schedule/utils.py
@@ -1,6 +1,8 @@
 from django.db.models import QuerySet
-
-from base.models import User
+from admin_bot.view_schedule import static_text
+from base.common_for_bots.static_text import DATE_INFO
+from base.common_for_bots.utils import get_time_info_from_tr_day
+from base.models import User, GroupTrainingDay, TrainingGroup
 
 
 def schedule_users_info(users: QuerySet[User]):
@@ -8,3 +10,99 @@ def schedule_users_info(users: QuerySet[User]):
         f"{i + 1}. {x['last_name']} {x['first_name']}"
         for i, x in enumerate(users.values('first_name', 'last_name').order_by('last_name'))
     ))
+
+
+class ViewSchedule:
+    @staticmethod
+    def _get_tr_day_status(tr_day: GroupTrainingDay) -> str:
+        if tr_day.is_individual:
+            return static_text.INDIVIDUAL_TRAIN
+        else:
+            if tr_day.tr_day_status == GroupTrainingDay.RENT_COURT_STATUS:
+                return static_text.RENT
+            else:
+                return static_text.MY_TRAIN
+
+    @staticmethod
+    def _get_group_name(tr_day: GroupTrainingDay) -> str:
+        return tr_day.group.name
+
+    @staticmethod
+    def _get_availability(tr_day: GroupTrainingDay) -> str:
+        return static_text.NO_TRAIN if not tr_day.is_available else ''
+
+    @staticmethod
+    def _get_group_players(tr_day: GroupTrainingDay) -> str:
+        group_players = tr_day.group.users.all().difference(tr_day.absent.all())
+        return f'{static_text.PLAYERS_FROM_GROUP}:\n{schedule_users_info(group_players)}'
+
+    @staticmethod
+    def _get_visitors(tr_day: GroupTrainingDay) -> str:
+        visitors = tr_day.visitors
+        if visitors.exists():
+            return f'{static_text.HAVE_COME_FROM_OTHERS}:\n{schedule_users_info(visitors)}'
+        return ''
+
+    @staticmethod
+    def _get_pay_visitors(tr_day: GroupTrainingDay) -> str:
+        pay_visitors = tr_day.pay_visitors
+        if pay_visitors.exists():
+            return f'{static_text.HAVE_COME_FOR_MONEY}:\n{schedule_users_info(pay_visitors)}'
+        return ''
+
+    @staticmethod
+    def _get_pay_bonus_visitors(tr_day: GroupTrainingDay) -> str:
+        pay_bonus_visitors = tr_day.pay_bonus_visitors
+        if pay_bonus_visitors.exists():
+            return f'{static_text.HAVE_COME_FOR_PAY_BONUS_LESSON}:\n{schedule_users_info(pay_bonus_visitors)}'
+        return ''
+
+    @staticmethod
+    def _get_absents(tr_day: GroupTrainingDay) -> str:
+        absents = tr_day.absent
+        if absents.exists():
+            return f'{static_text.ARE_ABSENT}:\n{schedule_users_info(absents)}'
+        return ''
+
+    @staticmethod
+    def _get_group_level(tr_day: GroupTrainingDay) -> str:
+        return f"{TrainingGroup.GROUP_LEVEL_DICT[tr_day.group.level]}"
+
+    @staticmethod
+    def _get_players_info(tr_day: GroupTrainingDay) -> str:
+        if tr_day.is_individual or tr_day.tr_day_status == GroupTrainingDay.RENT_COURT_STATUS:
+            return ''
+        else:
+            group_level = ViewSchedule._get_group_level(tr_day)
+            group_players = ViewSchedule._get_group_players(tr_day)
+            visitors = ViewSchedule._get_visitors(tr_day)
+            pay_visitors = ViewSchedule._get_pay_visitors(tr_day)
+            pay_bonus_visitors = ViewSchedule._get_pay_bonus_visitors(tr_day)
+            absents = ViewSchedule._get_absents(tr_day)
+            return '\n'.join([
+                group_level, group_players, visitors, pay_visitors, pay_bonus_visitors, absents
+            ])
+
+    @staticmethod
+    def _get_general_info(tr_day: GroupTrainingDay) -> str:
+        availability = ViewSchedule._get_availability(tr_day)
+        tr_day_status = ViewSchedule._get_tr_day_status(tr_day)
+        group_name = ViewSchedule._get_group_name(tr_day)
+        texts = list(filter)
+        return '\n'.join([
+            availability, tr_day_status, group_name
+        ])
+
+    @staticmethod
+    def _get_date_info(tr_day: GroupTrainingDay) -> str:
+        time_tlg, _, _, date_tlg, day_of_week, _, _ = get_time_info_from_tr_day(tr_day)
+        return DATE_INFO.format(date_tlg, day_of_week, time_tlg)
+
+    @staticmethod
+    def get_text(tr_day: GroupTrainingDay) -> str:
+        general_info = ViewSchedule._get_general_info(tr_day)
+        users_info = ViewSchedule._get_players_info(tr_day)
+        date_info = ViewSchedule._get_date_info(tr_day)
+        return '\n'.join([
+            general_info, users_info, date_info
+        ])

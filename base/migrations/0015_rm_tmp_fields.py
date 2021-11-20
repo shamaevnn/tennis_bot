0c1f5e267b477897ea2cb85198a6052094dde485
nikita <shamaev090299@gmail.com>
2021-11-20 17:51:18 +0300

    rm tmp fields

--- base/migrations/0015_rm_tmp_fields.py
+++ base/migrations/0015_rm_tmp_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0011_alertslog_player_tmp'),
+        ('base', '0014_tr_day_players'),
     ]
 
     operations = [

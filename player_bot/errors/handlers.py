89dc2b372083ea74d5d0876c3e443d6ddfe02dbc
nikita <shamaev090299@gmail.com>
2022-01-10 16:20:07 +0300

    don't send "Message is not modified" to error logs

--- player_bot/errors/handlers.py
+++ player_bot/errors/handlers.py
@@ -13,22 +13,22 @@ from tennis_bot.settings import ADMIN_CHAT_ID
 def send_stacktrace_to_tg_chat(update: Update, context: CallbackContext) -> None:
     player = Player.from_update(update)
 
-    logging.error("Exception while handling an update:", exc_info=context.error)
-
-    tb_list = traceback.format_exception(
-        None, context.error, context.error.__traceback__
-    )
-    tb_string = "".join(tb_list)
-
-    # Build the message with some markup and additional information about what happened.
-    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    # отправляем сообщение пользователю о том, чтобы он не волновался, что админы знают об ошибке
     context.bot.send_message(
         chat_id=player.tg_id,
         text=SOMETHING_WENT_WRONG_DONT_WORRY,
     )
 
-    message = f"<pre>{html.escape(tb_string)}</pre>"
-    admin_message = f"⚠️⚠️⚠️ for <a href='{player.get_link_to_django_admin}'>{player.tg_name}</a>:\n{message}"[
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+    tb_string = "".join(tb_list)
+    error_message = f"<pre>{html.escape(tb_string)}</pre>"
+
+    if "telegram.error.BadRequest: Message is not modified" in error_message:
+        return
+
+    admin_message = f"⚠️⚠️⚠️ for <a href='{player.get_link_to_django_admin}'>{player.tg_name}</a>:\n{error_message}"[
         :4090
     ]
     if ADMIN_CHAT_ID:

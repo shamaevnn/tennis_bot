464796429b20b50a8253d2e280bbba16da776775
Nikita Shamaev <nashamaye1@mts.ru>
2023-01-25 17:31:53 +0300

    rm parent bot info

--- run_parent_pooling.py
+++
@@ -1,9 +0,0 @@
-import os, django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tennis_bot.settings")
-django.setup()
-
-from parent_bot.main import main
-
-if __name__ == "__main__":
-    main()

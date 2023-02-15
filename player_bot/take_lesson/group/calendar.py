ab4a5361f6b6e167ecbb878a2b6fbfcc5d6ae345
Nikita Shamaev <nashamaye1@mts.ru>
2023-02-15 15:08:34 +0300

    fix: highlight dates in calendar if selected day without trainings

--- player_bot/take_lesson/group/calendar.py
+++ player_bot/take_lesson/group/calendar.py
@@ -1,6 +1,10 @@
 import calendar
 
-from datetime import date
+from datetime import date, datetime
+from typing import Tuple
+
+from telegram import InlineKeyboardMarkup
+
 from base.common_for_bots.static_text import from_eng_to_rus_day_week
 from base.common_for_bots.utils import DT_BOT_FORMAT, create_calendar
 from base.models import Player
@@ -9,23 +13,32 @@ from player_bot.take_lesson.group.manage_data import SELECT_PRECISE_GROUP_TIME
 from player_bot.take_lesson.group.query import get_potential_days_for_group_training
 
 
-def calendar_taking_group_lesson(player: Player, purpose, date_time):
+def calendar_taking_group_lesson(
+    player: Player, purpose: str, date_time: datetime
+) -> Tuple[str, InlineKeyboardMarkup]:
+    """
+    Игрок выбирает конкретный день из календаря.
+    Если в этот день есть доступные тренировки, предлагается выбрать конкретное время
+    Иначе предлагается выбрать другой день в календаре
+    """
     date_comparison = date(date_time.year, date_time.month, date_time.day)
-    training_days = get_potential_days_for_group_training(player, date=date_comparison)
-    highlight_dates = list(training_days.values_list("date", flat=True))
-    if training_days.exists():
-        buttons = construct_time_menu_for_group_lesson(
-            SELECT_PRECISE_GROUP_TIME, training_days, date_time, purpose
+    trainings_this_day = get_potential_days_for_group_training(player, date=date_comparison)
+    if trainings_this_day.exists():
+        markup = construct_time_menu_for_group_lesson(
+            SELECT_PRECISE_GROUP_TIME, trainings_this_day, date_time, purpose
         )
 
         day_of_week = calendar.day_name[date_time.weekday()]
-        text = f"Выбери время занятия на {date_time.strftime(DT_BOT_FORMAT)} ({from_eng_to_rus_day_week[day_of_week]})."
-        markup = buttons
+        rus_week_day = from_eng_to_rus_day_week[day_of_week]
+        string_formatted_date = date_time.strftime(DT_BOT_FORMAT)
+        text = f"Выбери время занятия на {string_formatted_date} ({rus_week_day})."
     else:
         text = (
             "Нет доступных тренировок в этот день, выбери другой.\n"
             "✅ -- дни, доступные для групповых тренировок"
         )
+        trainings = get_potential_days_for_group_training(player)
+        highlight_dates = list(trainings.values_list("date", flat=True))
         markup = create_calendar(
             purpose, date_time.year, date_time.month, highlight_dates
         )

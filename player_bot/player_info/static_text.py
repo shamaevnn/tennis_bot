384d1107520aae7c4a482fdf77fc693e4adddcd2
Onecelldm <pokesm.pe@gmail.com>
2023-04-10 13:46:59 +0300

    refactoring and add  Paymentt details

--- player_bot/player_info/static_text.py
+++ player_bot/player_info/static_text.py
@@ -1,3 +1,12 @@
 MY_DATA_BUTTON = "Мои данные"
 NO_PAYMENT_BUTTON = "❌НЕ ОПЛАЧЕНО❌"
 SUCCESS_PAYMENT = "✅ОПЛАЧЕНО✅"
+
+INTRO_INFO_TEMPLATE = "В данный момент ты {}\n\n"
+GROUP_INFO_TEMPLATE = "Твоя группа -- {}:\n{}\n\n"
+GAME_COUNT_INFO_TEMPLATE = "Количество отыгрышей: <b>{}</b>\n\n"
+
+SHOULD_PAY_INFO_TEMPLATE = "В этом месяце ({}) <b>нужно заплатить {} ₽ + {} ₽ за мячи.</b>\n"+\
+        "В следующем месяце ({}) <b>нужно заплатить {} ₽ + {} ₽ за мячи</b>."+\
+        "\n\n{}"
+

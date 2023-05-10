82d050bfff4f78cd0a3929f416b194ed5cd4d2f9
Onecelldm <pokesm.pe@gmail.com>
2023-05-10 22:44:19 +0300

    move functions

--- base/django_admin/utils.py
+++ base/django_admin/utils.py
@@ -2,7 +2,8 @@ from django.db.models import F
 
 from base.common_for_bots.utils import (
     get_actual_players_without_absent,
-    get_players_for_tr_day, get_dttm_info_for_tr_day,
+    get_players_for_tr_day,
+    get_dttm_info_for_tr_day,
 )
 from base.common_for_bots.tasks import broadcast_messages
 from base.models import GroupTrainingDay, Player, TrainingGroup
@@ -11,28 +12,19 @@ from base.django_admin.static_text import (
     CANCEL_TRAIN_PLUS_BONUS_LESSON_2,
     TRAIN_IS_AVAILABLE_CONGRATS,
 )
-from base.utils.change_available_status import change_tr_day_available_status, get_text_about_the_available_status_change
-
-# Отправка уведомлений об изменении статуса
-def send_alert_changing_tr_day_status(tr_day: GroupTrainingDay, available_status):
-    text = get_text_about_the_available_status_change(tr_day,available_status);
-    broadcast_messages(
-        chat_ids=list(get_players_for_tr_day(tr_day).values_list("tg_id", flat=True)),
-        message=text,
-        reply_markup=construct_main_menu(),
-    )
+from base.utils.change_available_status import (
+    change_tr_day_available_status,
+    get_text_about_the_available_status_change,
+)
 
 
-def change_tr_day_available_status_and_send_alert(tr_day: GroupTrainingDay, available_status):
-    change_tr_day_available_status(tr_day,available_status)
-    send_alert_changing_tr_day_status(tr_day,available_status);
-        
- 
 def send_alert_about_changing_tr_day_time(tr_day: GroupTrainingDay, text: str):
     absents = tr_day.absent.all()
     broadcast_messages(
         chat_ids=list(
-            get_players_for_tr_day(tr_day).union(absents).values_list("tg_id", flat=True)
+            get_players_for_tr_day(tr_day)
+            .union(absents)
+            .values_list("tg_id", flat=True)
         ),
         message=text,
         reply_markup=construct_main_menu(),
--- base/utils/change_available_status.py
+++ base/utils/change_available_status.py
@@ -1,65 +1,89 @@
-
 from django.db.models import F
-from base.common_for_bots.utils import get_actual_players_without_absent, get_dttm_info_for_tr_day
+from base.common_for_bots.utils import (
+    get_actual_players_without_absent,
+    get_dttm_info_for_tr_day,
+)
 from base.django_admin.static_text import (
-    CANCEL_TRAIN_PLUS_BONUS_LESSON_2, 
-    ERROR_UNKNOWN_AVAILABLE_STATUS, TRAIN_IS_AVAILABLE_CONGRATS)
+    CANCEL_TRAIN_PLUS_BONUS_LESSON_2,
+    ERROR_UNKNOWN_AVAILABLE_STATUS,
+    TRAIN_IS_AVAILABLE_CONGRATS,
+)
 
 from base.models import GroupTrainingDay, Player
+from base.common_for_bots.tasks import broadcast_messages
+from base.common_for_bots.utils import get_players_for_tr_day
+from player_bot.menu_and_commands.keyboards import construct_main_menu
 
 
 def change_tr_day_available_status(tr_day: GroupTrainingDay, available_status):
     if available_status == GroupTrainingDay.NOT_AVAILABLE:
-       
+
         players = get_actual_players_without_absent(tr_day)
         player_ids = players.values("id")
-        
+
         Player.objects.filter(id__in=player_ids).update(
-            bonus_lesson = F("bonus_lesson") + 1
+            bonus_lesson=F("bonus_lesson") + 1
         )
-         
+
     elif available_status == GroupTrainingDay.CANCELLED:
-       
+
         players = get_actual_players_without_absent(tr_day)
-        visitors = tr_day.visitors.all();
-        pay_bonus_visitors = tr_day.pay_bonus_visitors.all();
-        
+        visitors = tr_day.visitors.all()
+        pay_bonus_visitors = tr_day.pay_bonus_visitors.all()
+
         if tr_day.status == GroupTrainingDay.GROUP_ADULT_TRAIN:
             # обход всех  игроков
             for player in players:
-                # если ученик записался на занятие в счёт отыгрыша, то "отмена" не начисляется, 
+                # если ученик записался на занятие в счёт отыгрыша, то "отмена" не начисляется,
                 # а возвращается 1 "отыгрыш".
                 if player in visitors:
                     player.bonus_lesson += 1
                 # у игрока 0 отыгрышей и он записался на занятие за отыгрыш:
-                # в случае "отмены" занятия ни отмена, ни отыгрыш не добавляется. 
+                # в случае "отмены" занятия ни отмена, ни отыгрыш не добавляется.
                 elif player in pay_bonus_visitors:
                     continue
-                # если у ученика занятие по расписанию группы, то ему начисляется в личный кабинет одна "отмена". 
+                # если у ученика занятие по расписанию группы, то ему начисляется в личный кабинет одна "отмена".
                 else:
                     player.n_cancelled_lessons += 1
-                    
+
                 player.save()
-    else: 
+    else:
         raise ValueError(ERROR_UNKNOWN_AVAILABLE_STATUS.format(available_status))
-    
-def get_text_about_the_available_status_change(tr_day: GroupTrainingDay, available_status):
-    text =  ""
+
+
+def get_text_about_the_available_status_change(
+    tr_day: GroupTrainingDay, available_status
+):
+    text = ""
     date_info = get_dttm_info_for_tr_day(tr_day=tr_day)
-    
+
     if available_status == GroupTrainingDay.AVAILABLE:
         text = TRAIN_IS_AVAILABLE_CONGRATS.format(date_info=date_info)
-    
+
     elif available_status == GroupTrainingDay.NOT_AVAILABLE:
         text = CANCEL_TRAIN_PLUS_BONUS_LESSON_2.format(date_info=date_info)
-        
 
     elif available_status == GroupTrainingDay.CANCELLED:
         text = CANCEL_TRAIN_PLUS_BONUS_LESSON_2.format(date_info=date_info)
-    
+
     else:
         raise ValueError(ERROR_UNKNOWN_AVAILABLE_STATUS.format(available_status))
-    
+
     return text
-        
-    
+
+
+# Отправка уведомлений об изменении статуса
+def send_alert_changing_tr_day_status(tr_day: GroupTrainingDay, available_status):
+    text = get_text_about_the_available_status_change(tr_day, available_status)
+    broadcast_messages(
+        chat_ids=list(get_players_for_tr_day(tr_day).values_list("tg_id", flat=True)),
+        message=text,
+        reply_markup=construct_main_menu(),
+    )
+
+
+def change_tr_day_available_status_and_send_alert(
+    tr_day: GroupTrainingDay, available_status
+):
+    change_tr_day_available_status(tr_day, available_status)
+    send_alert_changing_tr_day_status(tr_day, available_status)

56ecb0a88bfa775c051396faf84ae7f8da394785
Nikita Shamaev <nashamaye1@mts.ru>
2023-06-09 18:24:00 +0300

    fixed price for rent

--- player_bot/take_lesson/rent/utils.py
+++ player_bot/take_lesson/rent/utils.py
@@ -4,6 +4,7 @@ from typing import Union
 def _get_price_for_renting(
     number_of_players: Union[str, int], duration_in_hours: float
 ) -> int:
-    price_per_hour = 500 + 100 * int(number_of_players)
+    # price_per_hour = 500 + 100 * int(number_of_players)
+    price_per_hour = 800  # todo: временное решение
     total_price = price_per_hour * duration_in_hours
     return int(total_price)
